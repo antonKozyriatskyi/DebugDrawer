b4a897261fa57c2264283ac8f8241bed642b4177
Anton <dev.anton.kozyriatskyi@gmail.com>
2018-08-07 23:25:46 +0300

    Add more samples of using the builder

--- example/src/main/java/antonkozyriatskyi/devdrawerexample/DslDrawerActivity.kt
+++ example/src/main/java/antonkozyriatskyi/devdrawerexample/DslDrawerActivity.kt
@@ -1,8 +1,12 @@
 package antonkozyriatskyi.devdrawerexample
 
+import android.graphics.Color
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.Gravity
+import android.view.View
+import android.view.ViewGroup.LayoutParams.MATCH_PARENT
+import android.widget.LinearLayout
 import antonkozyriatskyi.devdrawer.DevDrawer
 
 class DslDrawerActivity : AppCompatActivity() {
@@ -33,6 +37,57 @@ class DslDrawerActivity : AppCompatActivity() {
                     title = "Mock responses"
                     onCheckedChange { showToast("Mock responses enabled: $it") }
                 }
+            }
+
+            button {
+                title = "My button"
+            }
+
+            text {
+                title = "Custom text"
+            }
+
+            toggle {
+                title = "This is toggle"
+                onCheckedChange { showToast("Toggle toggled: $it") }
+            }
+
+            view {
+                View(this@DslDrawerActivity).also {
+                    it.setBackgroundColor(Color.GREEN)
+                    it.layoutParams = LinearLayout.LayoutParams(MATCH_PARENT, 300)
+                }
+            }
+
+            radioGroup {
+                radioButton {
+                    title = "Radio 1"
+                }
+                radioButton {
+                    title = "Radio 2"
+                }
+            }
+
+            text {
+                title = "Custom text"
+            }
+
+            divider()
+
+            text {
+                title = "Custom text"
+            }
+
+            divider(thickness = 4) {
+
+            }
+
+            text {
+                title = "Custom text"
+            }
+
+            divider {
+                thickness = 3
             }
         }
     }
