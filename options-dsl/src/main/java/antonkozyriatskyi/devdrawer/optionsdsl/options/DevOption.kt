2b5c97e3a06d99f01536d36e99cc7b47208373af
Anton <dev.anton.kozyriatskyi@gmail.com>
2018-08-07 23:22:57 +0300

    Make context protected

--- options-dsl/src/main/java/antonkozyriatskyi/devdrawer/optionsdsl/options/DevOption.kt
+++ options-dsl/src/main/java/antonkozyriatskyi/devdrawer/optionsdsl/options/DevOption.kt
@@ -3,6 +3,6 @@ package antonkozyriatskyi.devdrawer.optionsdsl.options
 import android.content.Context
 import android.view.View
 
-abstract class DevOption(context: Context) {
+abstract class DevOption(protected val context: Context) {
     abstract val view: View
 }
