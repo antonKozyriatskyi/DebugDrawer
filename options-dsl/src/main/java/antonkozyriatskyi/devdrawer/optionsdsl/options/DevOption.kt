1034c52bb0c92a264799d5b5bc1ffdaeda61be8a
Anton <dev.anton.kozyriatskyi@gmail.com>
2018-08-08 06:31:34 +0300

    Make `context` private Add `width` and `height` attributes

--- options-dsl/src/main/java/antonkozyriatskyi/devdrawer/optionsdsl/options/DevOption.kt
+++ options-dsl/src/main/java/antonkozyriatskyi/devdrawer/optionsdsl/options/DevOption.kt
@@ -3,6 +3,18 @@ package antonkozyriatskyi.devdrawer.optionsdsl.options
 import android.content.Context
 import android.view.View
 
-abstract class DevOption(protected val context: Context) {
+abstract class DevOption(val context: Context) {
     abstract val view: View
+
+    var width: Int
+        set(value) {
+            view.layoutParams.width = value
+        }
+        get() = view.layoutParams.width
+
+    var height: Int
+        set(value) {
+            view.layoutParams.height = value
+        }
+        get() = view.layoutParams.height
 }
