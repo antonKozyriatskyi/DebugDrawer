8fa7cd14af3da6bc581072e1a9c68f538d55987a
Anton Kozyriatskyi <anton.kozyriatskyi@gmail.com>
2018-08-08 12:45:41 +0300

    Add `id` property

--- options-dsl/src/main/java/antonkozyriatskyi/devdrawer/optionsdsl/options/DevOption.kt
+++ options-dsl/src/main/java/antonkozyriatskyi/devdrawer/optionsdsl/options/DevOption.kt
@@ -4,7 +4,14 @@ import android.content.Context
 import android.view.View
 
 abstract class DevOption(val context: Context) {
+
     abstract val view: View
+
+    var id: Int
+        set(value) {
+            view.id = value
+        }
+        get() = view.id
 
     var width: Int
         set(value) {
