d00d201dd30fd9bd5c8f1fb27f7e6a3858a2c891
Anton Kozyriatskyi <anton.kozyriatskyi@gmail.com>
2018-11-02 12:04:54 +0200

    Add `maxProgress` and `progress` properties to `SeekbarOption`

--- devdrawer/src/main/java/antonkozyriatskyi/devdrawer/options/SeekbarOption.kt
+++ devdrawer/src/main/java/antonkozyriatskyi/devdrawer/options/SeekbarOption.kt
@@ -7,6 +7,18 @@ class SeekbarOption(context: Context) : DevOption(context) {
 
     override val view = SeekBar(context)
 
+    var progress: Int
+        get() = view.progress
+        set(value) {
+            view.progress = value
+        }
+
+    var maxProgress: Int
+        get() = view.max
+        set(value) {
+            view.max = value
+        }
+
     fun onProgressChanged(listener: (progress: Int, fromUser: Boolean) -> Unit) {
         onProgressChanged(object : SeekBar.OnSeekBarChangeListener {
             override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
